107538cd13388088771a59bf348dbc64ea65f2dd
PHANETH PHO <phaneth2014@gmail.com>
2026-03-26 16:19:22 +0700

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-  origin: [process.env.LOCAL_URL,process.env.APP_URL, "https://khmer-public-holiday.onrender.com"], // allow your React frontend
+  origin: ["https://khmer-calendar.netlify.app", "https://khmer-public-holiday.onrender.com"], // allow your React frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
@@ -17,21 +17,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// IMPORTANT: include full path
-app.get("/api", (req, res) => {
-  res.status(200).json({ message: "Express server on Netlify is running..." });
-});
-
 app.use("/api", routes);
-
-const PORT = process.env.PORT || 3000;
-
-if (process.env.APP_LOCAL === 'local') {
-  app.listen(PORT, () => {
-    console.log(`✅ Local server running at http://localhost:${PORT}`);
-  });
-}
-
 
 export const handler = serverless(app);
