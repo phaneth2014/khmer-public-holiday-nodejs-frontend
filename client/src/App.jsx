070904f79e8558142ad164c8ade775fc1c64035a
PHANETH PHO <phaneth2014@gmail.com>
2026-03-27 17:51:11 +0700

    page udpate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Exchange from "./pages/Exchange";
+import Market from "./pages/Market";
+import Terms from './pages/Terms';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 import Footer from "./components/FooterPage";
 import "./App.css";
 
 import { ThemeProvider } from "./components/ThemeProvider";
+import Holiday from "./pages/Holiday";
 
 function App() {
 
@@ -14,6 +21,13 @@ function App() {
       <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/holiday" element={<Holiday />} />
+        <Route path="/exchange-rage" element={<Exchange />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </ThemeProvider>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,16 @@
-import React, {  useState } from "react";
-import {  NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import ThemeToggle from "./themeToggle";
 
 export default function Header() {
   const [sidebar, setSidebar] = useState(false);
- 
+
   const menuToggle = () => {
     setSidebar(!sidebar);
   };
-  
-  
+
   return (
-    <nav className="nav-header">     
+    <nav className="nav-header">
       <label className="menu-icon" onClick={menuToggle}>
         ☰
       </label>
@@ -22,92 +21,56 @@ export default function Header() {
         </label>
 
         <li>
-          <NavLink
-            to="/"
-            title="home"
-            id="pg-home" 
-          >
+          <NavLink to="/" title="home" id="pg-home">
             <i className="bi bi-house"></i> ប្រតិទិន
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/holiday"
-            title="holidays"
-           
-          >
+          <NavLink to="/holiday" title="holidays">
             <i className="bi bi-calendar"></i> ថ្ងៃឈប់សម្រាក
           </NavLink>
         </li>
 
         <li>
-          <NavLink
-            to="/exchange-rate"
-            title="exchange rate"
-            
-          >
+          <NavLink to="/exchange-rate" title="exchange rate">
             <i className="bi bi-currency-exchange"></i> អត្រាប្តូរប្រាក់
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/market"
-            title="market"
-           
-          >
+          <NavLink to="/market" title="market">
             <i className="bi bi-cart"></i> ទីផ្សារ/ហាងឆេង
           </NavLink>
         </li>
 
         <li>
-          <NavLink
-            to="/api-data"
-            title="api-data"
-            
-          >
+          <NavLink to="/api-data" title="api-data">
             <i className="bi bi-database"></i> ទិន្ន័យ API
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/about"
-            title="about"
-           
-          >
+          <NavLink to="/about" title="about">
             <i className="bi bi-info-circle"></i> អំពីយើង
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/terms"
-            title="terms"
-           
-          >
+          <NavLink to="/terms" title="terms">
             <i className="bi bi-key"></i> លក្ខខណ្ឌប្រើប្រាស់
           </NavLink>
         </li>
       </ul>
       <ul className="header-menu-right">
         <li>
-          <ThemeToggle/>
+          <ThemeToggle />
         </li>
 
         <li id="account-login">
-          <NavLink
-            to="/register"
-            title="Register"
-           
-          >
+          <NavLink to="/register" title="Register">
             <i className="bi bi-person"></i> ចុះឈ្មោះ
           </NavLink>
         </li>
 
         <li id="account-register">
-          <NavLink
-            to="/login"
-            title="Login"
-           
-          >
+          <NavLink to="/login" title="Login">
             <i className="bi bi-lock"></i> ចូល
           </NavLink>
         </li>
