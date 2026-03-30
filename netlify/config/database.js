26febc23a5c008ca6c1b274c7ca5072c642a95cd
PHANETH PHO <phaneth2014@gmail.com>
2026-03-30 13:20:32 +0700

    Update database.js

--- netlify/config/database.js
+++ netlify/config/database.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
+const connectionString = 'postgresql://portgresql:gTyCwggH5J650xVHKswV4U0DDAHE4ecS@dpg-d6ul7vsr85hc738tfok0-a.singapore-postgres.render.com/khmer_public_holiday_db';
+
 const pool = new Pool({
-  connectionString: process.env.RANDER_DB_URL,
+  connectionString: connectionString,
    ssl: { rejectUnauthorized: false } //if connecting to a cloud provider like Heroku or Azure
 });
 
