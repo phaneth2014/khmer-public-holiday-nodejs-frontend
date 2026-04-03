6acaa1e27efcb385127a3b5f317dc2ba473373eb
PHANETH PHO <phaneth2014@gmail.com>
2026-04-03 15:30:26 +0700

    update api function schedule

--- netlify/controllers/data.controller.js
+++ netlify/controllers/data.controller.js
@@ -95,11 +95,11 @@ export const getExchangeRate = async (req, res) => {
         if (date) {
             const pool = db.query ? db : db.default;
 
-            let result = await pool.query('SELECT * FROM exchange_rates WHERE date = $1', [date]);
+            let result = await pool.query('SELECT id, currency, rate, date::text as date, created_at, updated_at FROM exchange_rates WHERE date = $1', [date]);
             let data = result.rows;
 
             if (month) {
-                result = await pool.query('SELECT * FROM exchange_rates WHERE EXTRACT(MONTH FROM date) = $1 AND EXTRACT(YEAR FROM date) = $2', [month, year]);
+                result = await pool.query('SELECT id, currency, rate, date::text as date, created_at, updated_at FROM exchange_rates WHERE EXTRACT(MONTH FROM date) = $1 AND EXTRACT(YEAR FROM date) = $2', [month, year]);
                 data = result.rows;
             }
             const exchangeRateData = [{
--- netlify/functions/daily-task.js
+++ netlify/functions/daily-task.js
@@ -1,23 +1,23 @@
 import db from '../config/database.js';
-import { fetchAndStoreNBCRates } from "../controllers/RestApi.controller.js";
+// import { fetchAndStoreNBCRates } from "../controllers/RestApi.controller.js";
 
 export default async (request, context) => {
   try {
 
     const { next_run } = await request.json();
     console.log("Cron started. Next run scheduled for:", next_run);
-    const rates = await fetchAndStoreNBCRates();
+    // const rates = await fetchAndStoreNBCRates();
     // Your database logic
-    const rate = await db.query('SELECT id, currency, rate, date::text as date, created_at, updated_at FROM exchange_rates ORDER BY id DESC LIMIT 1');
-    if (rate.rows[0].date === rates[0].date) {
-      console.log("Latest exchange rate:", rate.rows[0].rate);
-    }else{
-      const query = await db.query(`
-                    INSERT INTO exchange_rates (currency, rate, date, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW()) RETURNING *;
-                `, [rates[0].currency, rates[0].rate, rates[0].date]);
+    // const rate = await db.query('SELECT id, currency, rate, date::text as date, created_at, updated_at FROM exchange_rates ORDER BY id DESC LIMIT 1');
+    // if (rate.rows[0].date === rates[0].date) {
+    //   console.log("Latest exchange rate:", rate.rows[0].rate);
+    // }else{
+    //   const query = await db.query(`
+    //                 INSERT INTO exchange_rates (currency, rate, date, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW()) RETURNING *;
+    //             `, [rates[0].currency, rates[0].rate, rates[0].date]);
                 
-      console.log(`Inserted ${query.rowCount} rows.`);
-    }
+    //   console.log(`Inserted ${query.rowCount} rows.`);
+    // }
 
     // No need to return a response body for scheduled functions
   } catch (error) {
