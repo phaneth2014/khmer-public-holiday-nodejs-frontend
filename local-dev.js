332a495b11ab2443f9fad031e32cb9962e15f30f
PHANETH PHO <phaneth2014@gmail.com>
2026-04-06 10:36:19 +0700

    Update local-dev.js

--- local-dev.js
+++ local-dev.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 
 import { verifyToken } from "./netlify/middleware/auth.middleware.js";
 import {
@@ -16,6 +17,12 @@ import { register,login,checkToken } from "./netlify/controllers/auth.controller
 
 const app = express();
 
+const privateCors = cors({
+  origin: ["http://localhost:4000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,8 +32,8 @@ app.get("/api/exchange-rate", getExchangeRate);
 app.post("/api/post-exchange-rate", fetchExchangeRates);
 app.get("/api/fetch-nbc-rates", fetchNBCRates);
 
-app.post('/api/register', register);
-app.post('/api/login', login);
+app.post('/api/register',privateCors , register);
+app.post('/api/login',privateCors, login);
 
 app.get('/api/neoconnection', requestHandler);
 app.get('/api/users-neon', getUsersList);
