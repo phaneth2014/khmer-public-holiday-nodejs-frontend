c64c236141564f543e07a5a5335cab1e9f99c1c0
PHANETH PHO <phaneth2014@gmail.com>
2026-07-06 21:26:43 +0700

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -50,8 +50,7 @@ export default function About() {
         បើអ្នកមានសំណួរ ឬចង់ផ្តល់មតិយោបល់ សូមទំនាក់ទំនងមកយើងខ្ញុំតាមរយៈអ៊ីមែល{" "}
         <a href="mailto:phaneth2014@gmail.com">phaneth2014@gmail.com</a>
       </p>
-      <DeviceMonitor />
-      <IPTracker/>
+
     </section>
   );
 }
