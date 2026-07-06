71a0406060c01cf82a8271f7475b35040c4ef353
PHANETH PHO <phaneth2014@gmail.com>
2026-07-06 20:48:24 +0700

    Update FooterPage.jsx

--- client/src/layouts/FooterPage.jsx
+++ client/src/layouts/FooterPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function footerPage() {
   return (
-    <div>©2024-{new Date().getFullYear()} by Khmer Calendar App</div>
-  )
+    <div>
+      ©2024-{new Date().getFullYear()} by Khmer Calendar App.  
+      <div>
+        <p>This site is powered by <a href="https://www.netlify.com">Netlify</a>.</p>
+        <a href="https://www.netlify.com">
+          <img
+            src="https://www.netlify.com/assets/badges/netlify-badge-color-accent.svg"
+            alt="Deploys by Netlify"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
